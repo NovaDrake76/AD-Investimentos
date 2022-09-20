4b53c571f3add1ba4d71419a09b0d3d0fdeefc6f
Nathan <novadrake76@gmail.com>
2022-09-20 18:23:12 -0300

    feat: multiple text corrections, Our Structure done, Advisors done, Gradient done

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,29 +1,31 @@
-import React from "react"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from "react-responsive-carousel"
-import Mountain from "../images/mountain.webp"
-import MountainMobile from "../images/mountainMobile.webp"
-import Equip from "../images/equip.webp"
-import EquipMobile from "../images/equipMobile.webp"
-import Office from "../images/office.webp"
-import OfficeMobile from "../images/officeMobile.webp"
-import MountainBack from "../images/backMountain.webp"
-import MountainBackMobile from "../images/backMountainMobile.webp"
-import Card1 from "../images/card1.webp"
-import Card2 from "../images/card2.webp"
-import History from "../images/history.webp"
-import { FaMedal, FaUserGraduate, FaHandshake } from "react-icons/fa"
-import { MdSecurity, MdTimeline, MdOutlineMailOutline } from "react-icons/md"
-import { IoRocketSharp } from "react-icons/io5"
-import { RiMoneyDollarCircleLine } from "react-icons/ri"
-import { AiOutlineWhatsApp } from "react-icons/ai"
-import { BsLinkedin, BsFacebook, BsInstagram } from "react-icons/bs"
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import Mountain from "../images/mountain.webp";
+import MountainMobile from "../images/mountainMobile.webp";
+import Equip from "../images/equip.webp";
+import EquipMobile from "../images/equipMobile.webp";
+import Office from "../images/office.webp";
+import OfficeMobile from "../images/officeMobile.webp";
+import MountainBack from "../images/backMountain.webp";
+import MountainBackMobile from "../images/backMountainMobile.webp";
+import Card1 from "../images/card1.webp";
+import Card2 from "../images/card2.webp";
+import History from "../images/history.webp";
+import Alvaro from "../images/alvaro.webp";
+import { FaMedal, FaUserGraduate, FaHandshake } from "react-icons/fa";
+import { MdSecurity, MdTimeline, MdOutlineMailOutline } from "react-icons/md";
+import { IoRocketSharp } from "react-icons/io5";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { BsLinkedin, BsFacebook, BsInstagram } from "react-icons/bs";
 
 let renderCarouselImages,
   renderAdvantages,
   renderCards,
   renderAboutUs,
-  renderContact
+  renderAdvisors,
+  renderContact;
 
 const Landing = () => {
   const carouselImages = [
@@ -46,12 +48,12 @@ const Landing = () => {
       image: Equip,
       imageMobile: EquipMobile,
       content: (
-        <div className="flex flex-col w-4/5 items-center gap-4">
+        <div className="flex flex-col items-center w-4/5 gap-4">
           <span className="flex text-xl md:text-5xl text-white font-extralight font-['Raleway']">
             AD INVESTIMENTOS
           </span>
           <h2 className="text-2xl md:text-[64px] font-bold font-['Oswald'] text-white leading-tight">
-            INVISTA COM ACESSORES EM INVESTIMENTOS QUE SÃO ESPECIALISTAS.
+            INVISTA COM ASSESSORES QUE SÃO ESPECIALISTAS EM INVESTIMENTOS.
             <span className="text-adblue"> SEM CUSTO.</span>
           </h2>
         </div>
@@ -61,7 +63,7 @@ const Landing = () => {
       image: Office,
       imageMobile: OfficeMobile,
       content: (
-        <div className="flex flex-col w-4/5 items-center gap-4">
+        <div className="flex flex-col items-center w-4/5 gap-4">
           <h2 className="flex flex-col gap-10 text-2xl md:text-[64px] font-bold font-['Oswald'] text-white leading-tight text-start">
             NÓS ANALISAMOS INVESTIMENTOS TODOS OS DIAS PARA TE ENTREGAR APENAS
             <span className="text-adblue"> O MELHOR.</span>
@@ -69,7 +71,7 @@ const Landing = () => {
         </div>
       ),
     },
-  ]
+  ];
 
   const advantages = [
     {
@@ -96,13 +98,13 @@ const Landing = () => {
         "Seu especialista estará constantemente analisando o mercado, sugerindo ajustes que precisam ser feitos para acompanhar as melhores oportunidades do mercado.",
       icon: IoRocketSharp,
     },
-  ]
+  ];
 
   const cards = [
     {
-      name: "Consultoria Financeira",
+      name: "Análise das Aplicações ",
       description:
-        "Conte com o acompanhamento de um especialista AD para te ajudar a melhorar seus resultados.",
+        "Analise suas aplicações em outros bancos sem custo, e descubra se é possível melhorar seus resultados.",
       image: Card1,
     },
     {
@@ -111,7 +113,7 @@ const Landing = () => {
         "Acompanhamento de investimentos, planejamento financeiro e muito mais.",
       image: Card2,
     },
-  ]
+  ];
 
   const about = [
     {
@@ -121,15 +123,16 @@ const Landing = () => {
     },
     {
       Number: "500",
-      Description: "Clientes atendidos",
+      Description: "Clientes investidores atendidos em bancos",
       Icon: FaHandshake,
     },
     {
-      Number: "R$300",
-      Description: "Milhões investidos",
+      Number: "300",
+      Description:
+        "Milhões de reais acessorados em bancos comerciais e de investimentos",
       Icon: RiMoneyDollarCircleLine,
     },
-  ]
+  ];
 
   const contacts = [
     {
@@ -157,7 +160,16 @@ const Landing = () => {
       icon: BsInstagram,
       link: "https://www.instagram.com/adinvestimentos.oficial/",
     },
-  ]
+  ];
+
+  const advisors = [
+    {
+      name: "Álvaro Dias",
+      image: Alvaro,
+      description:
+        "CEO e fundador da AD Investimentos, com mais de 5 anos de experiência no mercado financeiro. Possui formação em Administração de Empresas pela Universidade Federal de Minas Gerais (UFMG) e é CEA (Certificado de Especialista em Assessoria de Investimentos) pela ANBIMA.",
+    },
+  ];
 
   renderAdvantages = advantages.map((advantage, index) => {
     return (
@@ -176,13 +188,13 @@ const Landing = () => {
           {advantage.description}
         </span>
       </div>
-    )
-  })
+    );
+  });
 
   renderCarouselImages = carouselImages.map((item) => {
     return (
       <div key={item.image}>
-        <div className="absolute h-full flex justify-center items-center">
+        <div className="absolute flex items-center justify-center h-full">
           {item.content}
         </div>
         <img
@@ -193,17 +205,17 @@ const Landing = () => {
           className="min-w-screen"
         />
       </div>
-    )
-  })
+    );
+  });
 
   renderCards = cards.map((card, index) => {
     return (
-      <div key={index} className="flex flex-col md:flex-row gap-5">
+      <div key={index} className="flex flex-col gap-5 md:flex-row">
         <div className="flex items-center justify-center ">
           <img
             src={card.image}
             alt={card.name}
-            className="object-cover h-52 md:h-72 px-4 lg:px-0"
+            className="object-cover px-4 h-52 md:h-72 lg:px-0"
           />
         </div>
         <div className="flex flex-col p-4 gap-5 md:max-w-[260px] ">
@@ -215,8 +227,8 @@ const Landing = () => {
           </span>
         </div>
       </div>
-    )
-  })
+    );
+  });
 
   renderAboutUs = about.map((item, index) => {
     return (
@@ -227,16 +239,16 @@ const Landing = () => {
         <div className="w-20 h-20 text-[#2C2F33]">
           <item.Icon className="text-6xl" />
         </div>
-        <div className="flex flex-col gap-4 items-center">
-          <span className="font-['Oswald'] text-2xl">+{item.Number}</span>
+        <div className="flex flex-col items-center gap-4">
+          <span className="font-['Oswald'] text-3xl">+{item.Number}</span>
           <div className="flex bg-adblue w-[50px] h-[1px]" />
           <span className="font-['Raleway'] font-bold text-lg">
             {item.Description}
           </span>
         </div>
       </div>
-    )
-  })
+    );
+  });
 
   renderContact = contacts.map((contact, index) => {
     return (
@@ -253,41 +265,68 @@ const Landing = () => {
           {contact.name}
         </span>
       </div>
-    )
-  })
+    );
+  });
+
+  renderAdvisors = advisors.map((advisor, index) => {
+    return (
+      <div key={index} className="flex flex-col items-center max-w-sm gap-4">
+        <div className="flex items-center justify-start w-full gap-12">
+          <img
+            src={advisor.image}
+            alt={advisor.name}
+            className="w-[100px] h-[100px] rounded-full"
+          />
+          <span className="font-['Raleway'] font-bold text-xl text-[#4c4c4c]">
+            {advisor.name}
+          </span>
+        </div>
+        <p className="font-['Raleway']  text-[#4d4d4d] text-start">
+          {advisor.description}
+        </p>
+      </div>
+    );
+  });
 
   function renderOurTitle(our, title) {
     return (
-      <div className="flex flex-col border-adblue border-l-2 px-5 text-start ">
+      <div className="flex flex-col px-5 border-l-2 border-adblue text-start ">
         <span className="text-2xl text-adblue font-['Raleway'] font-semibold">
           {our}
         </span>
         <span className="text-5xl font-['Oswald'] font-bold">{title}</span>
       </div>
-    )
+    );
   }
 
   return (
     <div className="flex flex-col ">
       <Carousel
         autoPlay={true}
-        interval={4000}
+        interval={3000}
         showStatus={false}
-        showArrows={true}
+        showArrows={false}
         infiniteLoop={true}
         showThumbs={false}
-        className="border-adblue border-b-8"
+        className="border-b-8 border-adblue"
       >
         {renderCarouselImages}
       </Carousel>
       <div className="flex justify-center mt-20">
-        <div className="flex flex-col gap-20 w-full max-w-6xl">
+        <div className="flex flex-col w-full max-w-6xl gap-20">
           {renderOurTitle("NOSSAS", "VANTAGENS")}
-          <div className="flex flex-col md:flex-row gap-9 justify-center">
+          <div className="flex flex-col justify-center md:flex-row gap-9">
             {renderAdvantages}
           </div>
           {renderOurTitle("NOSSOS", "PRODUTOS")}
-          <div className="flex flex-col md:flex-row gap-5">{renderCards}</div>
+          <div className="flex flex-col gap-5 md:flex-row">{renderCards}</div>
+          {renderOurTitle("NOSSA", "ESTRUTURA")}
+          <p className="text-start text-lg font-['Raleway'] text-[#4c4c4c] font-bold leading-8 max-w-3xl px-5">
+            Localizado na cidade de Três Lagoas-MS, contamos com prédio
+            comercial no centro da cidade localizado na AV. Olinto Mancini 878,
+            para melhor atendê-los. Venha tomar um café conosco e analisar suas
+            aplicações. Qualidade garantida!
+          </p>
           {renderOurTitle("CONTE", "CONOSCO")}
           <div className="flex ">
             <p className="text-start text-lg font-['Raleway'] text-[#4c4c4c] font-bold leading-8 max-w-3xl px-5">
@@ -308,9 +347,10 @@ const Landing = () => {
           </div>
         </div>
       </div>
+
       <div className="flex justify-center">
         <div className="absolute flex flex-col gap-4 md:gap-14">
-          <div className=" h-2/4 flex flex-col gap-4 justify-center items-center max-w-4xl mt-14">
+          <div className="flex flex-col items-center justify-center max-w-4xl gap-4 h-2/4 mt-14">
             <h2 className="text-2xl md:text-[64px] font-bold font-['Oswald'] text-white leading-tight ">
               <span>VAMOS TRAZER </span>
               <span className="text-adblue">
@@ -318,7 +358,7 @@ const Landing = () => {
               </span>
             </h2>
           </div>
-          <div className="flex flex-col items-center md:items-stretch md:flex-row gap-2 md:justify-around w-full">
+          <div className="flex flex-col items-center w-full gap-2 md:items-stretch md:flex-row md:justify-around">
             {renderAboutUs}
           </div>
         </div>
@@ -331,7 +371,18 @@ const Landing = () => {
         />
       </div>
       <div className="flex justify-center mt-20 ">
-        <div className="flex flex-col justify-start  max-w-6xl w-full gap-4">
+        <div className="flex flex-col justify-start w-full max-w-6xl gap-4">
+          <div id="contatos" className="flex flex-col gap-14">
+            {renderOurTitle("NOSSOS", "ACESSORES")}
+            <div className="flex gap-5 "></div>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+            {renderAdvisors}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center mt-20 ">
+        <div className="flex flex-col justify-start w-full max-w-6xl gap-8">
           <div id="contatos" className="flex flex-col gap-14">
             {renderOurTitle("NOSSOS", "CONTATOS")}
             <div className="flex flex-col gap-5 px-6">
@@ -344,13 +395,41 @@ const Landing = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row gap-15 md:gap-20 justify-center items-center mb-10">
+          <div className="flex flex-col items-center justify-center mb-10 md:flex-row gap-15 md:gap-20">
             {renderContact}
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+      <div
+        className="flex items-center justify-center w-full h-40"
+        style={{
+          background:
+            "linear-gradient(to right, rgba(126, 9, 235, 0.8) 0%, rgba(114, 17, 215, 0.8) 17%, #373873 100%",
+        }}
+      >
+        <div className="flex justify-between w-full max-w-6xl">
+          <div className="flex flex-col text-white text-start">
+            <h3 className="font-['Raleway'] font-medium  text-lg">
+              Pare de esperar
+            </h3>
 
-export default Landing
+            <h3 className="font-['Raleway']  text-3xl">
+              Aplique seu dinheiro conosco agora mesmo!
+            </h3>
+          </div>
+          <a
+            href="https://api.whatsapp.com/send?phone=5567993466245"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="flex text-white border-2 gap-3 border-white font-['Raleway'] font-medium items-center py-4 px-3 text-lg hover:border-adblue hover:text-adblue transition-all duration-500 ease-in-out">
+              <AiOutlineWhatsApp /> Fale com um especialista AD
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
